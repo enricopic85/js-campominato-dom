7fdd5956da09df4a87e437b854691084e60bf367
enricopic85 <enricodev85@gmail.com>
2021-12-20 17:20:54 +0100

    creazione punteggio

--- css/js/script.js
+++ css/js/script.js
@@ -2,21 +2,32 @@
 function createEvent(box,color){
     box.addEventListener('click',function(){
         this.classList.add(color);
+        if (color==="red") {
+            alert(`hai perso ed hai totalizzato ${punteggio}`);
+
+        } else{
+            punteggio++;
+        }
     })
 }
 
-function createNewBox(noRepeat,griglia,box,i,min,max){
+function createNewBox(noRepeat,griglia,box,i){
     let newBox=document.createElement('div');
     newBox.className=box;
     newBox.innerText=i;
     griglia.append(newBox);
     if (noRepeat.includes(i)) {
-        createEvent(newBox,"red")
+        createEvent(newBox,"red");
     } else{
         createEvent(newBox,"azure")
     }
 }
-
+function punteggioMax(max){
+    let totale=max-16;
+    if (totale===punteggio) {
+        alert("complimenti hai terminato la partita!")
+    }
+}
 //il computer deve generare 16 numeri casuali nello stesso range di difficoltà prescelta:le bombe.
 //I numeri nella lista delle bombe non possono essere duplicati.
 // let random= (min,max) => Math.floor( Math.random() * (max-min) + min);
@@ -31,7 +42,8 @@ function noRepeatNumber(min,max){
     }
     return numbers;
 }
-console.log(noRepeatNumber(1,100));
+
+let punteggio=0;
 let griglia1=document.getElementById("griglia1");
 let scelta1=document.getElementById("level1");
 let scelta2=document.getElementById("level2");
@@ -40,24 +52,27 @@ let scelta3=document.getElementById("level3");
 
 scelta1.addEventListener('click',function(){
         griglia1.innerHTML='';
+        punteggioMax(100);
         let bombe=noRepeatNumber(1,100)
         for(i=1; i <= 100; i++){
-        createNewBox(bombe,griglia1,'box1',i,1,100);
+        createNewBox(bombe,griglia1,'box1',i);
 
     }
  });
  scelta2.addEventListener('click',function(){
     griglia1.innerHTML='';
-    let bombe=noRepeatNumber(1,81)
+    punteggioMax(81);
+    let bombe=noRepeatNumber(1,81);
     for(i=1; i <= 81; i++){
-    createNewBox(bombe,griglia1,'box2',i,1,81);
+    createNewBox(bombe,griglia1,'box2',i);
 }
 });
 scelta3.addEventListener('click',function(){
     griglia1.innerHTML='';
+    punteggioMax(49);
     let bombe=noRepeatNumber(1,49)
     for(i=1; i <= 49; i++){
-    createNewBox(bombe,griglia1,'box3',i,1,49);
+    createNewBox(bombe,griglia1,'box3',i);
 }
 });
 

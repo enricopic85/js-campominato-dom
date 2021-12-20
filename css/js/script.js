fc548e0ffc678755ae0531692f96774ad52e1fea
enricopic85 <enricodev85@gmail.com>
2021-12-20 14:57:24 +0100

    tentativo creazione quadrati con bombe

--- css/js/script.js
+++ css/js/script.js
@@ -1,11 +1,34 @@
-function createNewBox(griglia,box,max){
+function createEvent(box,color){
+    box.addEventListener('click',function(){
+        this.classList.add(color);
+    })
+}
+
+function createNewBox(griglia,box,i,numMin,numMax){
     let newBox=document.createElement('div');
     newBox.className=box;
-    newBox.innerText=max;
+    newBox.innerText=i;
     griglia.append(newBox);
-    newBox.addEventListener('click',function(){
-        this.classList.add("azure");
-    })
+    let randoms=random(numMin,numMax)
+    if (i===noRepeatNumber(randoms)) {
+        createEvent(newBox,"red");
+    } else{
+        createEvent(newBox,"azure");
+    }
+}
+//il computer deve generare 16 numeri casuali nello stesso range di difficoltà prescelta:le bombe.
+//I numeri nella lista delle bombe non possono essere duplicati.
+let random= (min,max) => Math.floor( Math.random() * (max-min) + min);
+
+function noRepeatNumber(randoms){
+    const numbers=[];
+    while (numbers.length < 16) {
+        const random=randoms;
+        if (!numbers.includes(random)) {
+            numbers.push(random);
+        }
+    }
+    return numbers;
 }
 
 let griglia1=document.getElementById("griglia1");
@@ -17,7 +40,8 @@ let scelta3=document.getElementById("level3");
 scelta1.addEventListener('click',function(){
         griglia1.innerHTML='';
         for(i=1; i <= 100; i++){
-        createNewBox(griglia1,'box1',i);
+        createNewBox(griglia1,'box1',i,1,100);
+            
     }
  });
  scelta2.addEventListener('click',function(){
@@ -33,14 +57,5 @@ scelta3.addEventListener('click',function(){
 }
 });
 
-let random=(min,max) => Math.floor( Math.random() * (max-min) + min);
 
-const numbers=[];
-while (numbers.length < 16) {
-    const random=random(1,16);
-    if (!numbers.includes(random)) {
-        numbers.push(random);
-    }
-    
-}
-console.log(numbers)
+
